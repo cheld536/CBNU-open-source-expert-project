c30d76630930e5be3cc96e86b4ec1ad6d743e272
DOUBLE-J04 <80048520+DOUBLE-J04@users.noreply.github.com>
2022-06-13 02:32:34 +0900

    Update Api.java

--- src/main/java/com/example/api/Api.java
+++ src/main/java/com/example/api/Api.java
@@ -8,8 +8,7 @@ import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 public interface Api {
-    //http://113.198.137.183:7998/
-    String BASE_URL = "https://ec2-13-209-74-60.ap-northeast-2.compute.amazonaws.com:3000";
+    String BASE_URL = "";
 
     @GET("user/users")
     Call<List<Results>> getuserInfo();
